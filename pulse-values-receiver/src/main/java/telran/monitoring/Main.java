28ec84657b522a657004035aa36f6623eac955a2
Daniel <gromdan90@gmail.com>
2025-03-03 19:28:42 +0200

    class work session 030225

---
+++ pulse-values-receiver/src/main/java/telran/monitoring/Main.java
@@ -0,0 +1,20 @@
+package telran.monitoring;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketAddress;
+
+public class Main {
+    private static final int PORT = 3500;
+    private static final int MAX_SIZE = 1500;
+    public static void main(String[] args) throws Exception {
+        DatagramSocket socket = new DatagramSocket(PORT);
+        byte[] buffer = new byte[MAX_SIZE];
+        while (true) {   
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            socket.receive(packet);
+            System.out.println(new String(packet.getData()));
+            socket.send(packet);
+        }
+    };
+}
