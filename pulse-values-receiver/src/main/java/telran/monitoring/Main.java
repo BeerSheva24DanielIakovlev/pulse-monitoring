6735b0c0a72fc279fc7cb196ccb9afe15989b299
Daniel <gromdan90@gmail.com>
2025-03-04 12:19:08 +0200

    solution by Yuri

--- pulse-values-receiver/src/main/java/telran/monitoring/Main.java
+++ pulse-values-receiver/src/main/java/telran/monitoring/Main.java
@@ -1,7 +1,5 @@
 package telran.monitoring;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
@@ -10,53 +8,41 @@ import telran.monitoring.logging.Logger;
 import telran.monitoring.logging.LoggerStandard;
 
 public class Main {
-    private static final int PORT = 3500;
+
+    private static final int PORT = 5000;
     private static final int MAX_SIZE = 1500;
-    private static final String PULSE_LOG_FILE = "pulse_log.txt";
-    private static final String ERROR_LOG_FILE = "error_log.txt";
-    private static final int LOGGED_PATIENT_ID = 3;
-    private static final int HIGHT_BAD_PULSE = 120;
-    private static final int LOW_BAD_PULSE = 60;
-    private static final int HIGHT_CRIT = 160;
-    private static final int LOW_CRIT = 50;
+    private static final int WARNING_LOG_VALUE = 220;
+    private static final int ERROR_LOG_VALUE = 230;
     static Logger logger = new LoggerStandard("receiver");
 
     public static void main(String[] args) throws Exception {
-        @SuppressWarnings("resource")
         DatagramSocket socket = new DatagramSocket(PORT);
+
         byte[] buffer = new byte[MAX_SIZE];
         while (true) {
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
             socket.receive(packet);
-            
-            String receivedData = new String(packet.getData(), 0, packet.getLength());
-            SensorData sensorData = SensorData.of(receivedData);
-            int pulseValue = sensorData.value();
-            
-            if (sensorData.patientId() == LOGGED_PATIENT_ID) {
-                logger.log("finest", "Received data: " + receivedData);
-                writeToFile(PULSE_LOG_FILE, receivedData);
-                
-                if (pulseValue > HIGHT_CRIT || pulseValue < LOW_CRIT) {
-                    String errorMsg = "Critical pulse: " + pulseValue;
-                    logger.log("severe", errorMsg);
-                    writeToFile(ERROR_LOG_FILE, errorMsg);
-                } else if (pulseValue > HIGHT_BAD_PULSE || pulseValue < LOW_BAD_PULSE) {
-                    String warningMsg = "Bad pulse: " + pulseValue;
-                    logger.log("warning", warningMsg);
-                    writeToFile(ERROR_LOG_FILE, warningMsg);
-                }
-            }
-            
+            String jsonStr = new String(packet.getData());
+            logger.log("finest", jsonStr);
+            logPulseValue(jsonStr);
+
             socket.send(packet);
         }
     }
 
-    private static void writeToFile(String fileName, String data) {
-        try (FileWriter writer = new FileWriter(fileName, true)) {
-            writer.write(data + "\n");
-        } catch (IOException e) {
-            logger.log("severe", "Error writing to file: " + fileName);
+    private static void logPulseValue(String jsonStr) {
+        SensorData sensorData = SensorData.of(jsonStr);
+        int value = sensorData.value();
+        if (value >= WARNING_LOG_VALUE && value <= ERROR_LOG_VALUE) {
+            logValue("warning", sensorData);
+        } else if(value > ERROR_LOG_VALUE) {
+            logValue("error", sensorData);
         }
     }
+
+    private static void logValue(String level, SensorData sensorData) {
+        logger.log(level, String.format("patient %d has pulse value greater than %d", sensorData.patientId(),
+                level.equals("warning") ? WARNING_LOG_VALUE : ERROR_LOG_VALUE));
+    }
+
 }
