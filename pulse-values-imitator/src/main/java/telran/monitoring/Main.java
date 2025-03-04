38214e7da1ea722e93d6547cf6e077474cdd4239
Daniel <gromdan90@gmail.com>
2025-03-04 12:18:19 +0200

    my solution

--- pulse-values-imitator/src/main/java/telran/monitoring/Main.java
+++ pulse-values-imitator/src/main/java/telran/monitoring/Main.java
@@ -2,63 +2,80 @@ package telran.monitoring;
 
 import java.net.*;
 import java.util.Random;
-import java.util.stream.IntStream;
 
 import telran.monitoring.api.SensorData;
-import telran.monitoring.logging.Logger;
-import telran.monitoring.logging.LoggerStandard;
 
 public class Main {
-    static Logger logger = new LoggerStandard("imitator");
     static final int MIN_PULSE_VALUE = 40;
     static final int MAX_PULSE_VALUE = 240;
-    static final long TIMEOUT_SEND = 500;
-    static final int TIMEOUT_RESPONCE = 1000;
+    static final long TIMEOUT_SEND = 2000;
     static final String DEFAULT_HOST = "localhost";
     static final int DEFAULT_PORT = 3500;
     static final int DEFAULT_N_PATIENTS = 10;
-    static final int DEFAULT_N_PACKETS = 50;
-    static DatagramSocket socket = null;
+    static final double JUMP_PROB = 0.1;
+    static final int MIN_JUMP_PERCENT = 10;
+    static final int MAX_JUMP_PERCENT = 50;
+    static final double JUMP_POSITIVE_PROB = 0.7;
+    static final int NORMAL_MIN = 60;
+    static final int NORMAL_MAX = 80;
+
+    static DatagramSocket socket;
+    static Random random = new Random();
+    static int[] lastPulseValues = new int[DEFAULT_N_PATIENTS + 1];
 
     public static void main(String[] args) throws Exception {
         socket = new DatagramSocket();
-        socket.setSoTimeout(TIMEOUT_RESPONCE);
-        IntStream.rangeClosed(1, DEFAULT_N_PACKETS).forEach(Main::send);
-    };
+        
+        for (int i = 1; i <= DEFAULT_N_PATIENTS; i++) {
+            lastPulseValues[i] = getRandomNumber(NORMAL_MIN, NORMAL_MAX);
+        }
+        
+        while (true) {
+            for (int patientId = 1; patientId <= DEFAULT_N_PATIENTS; patientId++) {
+                send(patientId);
+            }
+            Thread.sleep(TIMEOUT_SEND);
+        }
+    }
 
-    static void send(int i) {
-        SensorData sensor = getRandomSensorData(i);
+    static void send(int patientId) {
+        SensorData sensor = getRealisticSensorData(patientId);
         String jsonStr = sensor.toString();
+        System.out.println("Generated: " + jsonStr);
         try {
             udpSend(jsonStr);
         } catch (Exception e) {
             e.printStackTrace();
-        };
-    };
+        }
+    }
 
     private static void udpSend(String jsonStr) throws Exception {
-        logger.log("finest", String.format("data to be send is %s", jsonStr));
         byte[] bufferSend = jsonStr.getBytes();
         DatagramPacket packet =
-            new DatagramPacket(bufferSend, bufferSend.length,
-            InetAddress.getByName(DEFAULT_HOST), DEFAULT_PORT);
+                new DatagramPacket(bufferSend, bufferSend.length,
+                        InetAddress.getByName(DEFAULT_HOST), DEFAULT_PORT);
         socket.send(packet);
-        socket.receive(packet);
-        if (!jsonStr.equals(new String(packet.getData()))) {
-            throw new Exception("received packet doesn't equal the sent one");
-        };
-    };
+    }
 
-    private static SensorData getRandomSensorData(int i) {
-        long patientId = getRandomNumber(1, DEFAULT_N_PATIENTS);
-        int value = getRandomNumber(MIN_PULSE_VALUE, MAX_PULSE_VALUE);
+    private static SensorData getRealisticSensorData(int patientId) {
+        int value = applyPulseJump(patientId);
+        lastPulseValues[patientId] = value;
         long timestamp = System.currentTimeMillis();
-        SensorData res = new SensorData(patientId, value, timestamp);
+        return new SensorData(patientId, value, timestamp);
+    }
 
-        return res;
+    private static int applyPulseJump(int patientId) {
+        if (random.nextDouble() < JUMP_PROB) {
+            int jumpPercent = getRandomNumber(MIN_JUMP_PERCENT, MAX_JUMP_PERCENT);
+            int jumpValue = (lastPulseValues[patientId] * jumpPercent) / 100;
+            boolean positiveJump = random.nextDouble() < JUMP_POSITIVE_PROB;
+            int newValue = positiveJump ? lastPulseValues[patientId] + jumpValue : lastPulseValues[patientId] - jumpValue;
+            return Math.max(MIN_PULSE_VALUE, Math.min(newValue, MAX_PULSE_VALUE));
+        }
+        return lastPulseValues[patientId];
     }
 
     private static int getRandomNumber(int minValue, int maxValue) {
-        return new Random().nextInt(minValue, maxValue + 1);
-    };
-};
+        return random.nextInt(minValue, maxValue + 1);
+    }
+}
--- pulse-values-receiver/src/main/java/telran/monitoring/Main.java
+++ pulse-values-receiver/src/main/java/telran/monitoring/Main.java
@@ -1,20 +1,62 @@
 package telran.monitoring;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.SocketAddress;
+
+import telran.monitoring.api.SensorData;
+import telran.monitoring.logging.Logger;
+import telran.monitoring.logging.LoggerStandard;
 
 public class Main {
     private static final int PORT = 3500;
     private static final int MAX_SIZE = 1500;
+    private static final String PULSE_LOG_FILE = "pulse_log.txt";
+    private static final String ERROR_LOG_FILE = "error_log.txt";
+    private static final int LOGGED_PATIENT_ID = 3;
+    private static final int HIGHT_BAD_PULSE = 120;
+    private static final int LOW_BAD_PULSE = 60;
+    private static final int HIGHT_CRIT = 160;
+    private static final int LOW_CRIT = 50;
+    static Logger logger = new LoggerStandard("receiver");
+
     public static void main(String[] args) throws Exception {
+        @SuppressWarnings("resource")
         DatagramSocket socket = new DatagramSocket(PORT);
         byte[] buffer = new byte[MAX_SIZE];
-        while (true) {   
+        while (true) {
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
             socket.receive(packet);
-            System.out.println(new String(packet.getData()));
+            
+            String receivedData = new String(packet.getData(), 0, packet.getLength());
+            SensorData sensorData = SensorData.of(receivedData);
+            int pulseValue = sensorData.value();
+            
+            if (sensorData.patientId() == LOGGED_PATIENT_ID) {
+                logger.log("finest", "Received data: " + receivedData);
+                writeToFile(PULSE_LOG_FILE, receivedData);
+                
+                if (pulseValue > HIGHT_CRIT || pulseValue < LOW_CRIT) {
+                    String errorMsg = "Critical pulse: " + pulseValue;
+                    logger.log("severe", errorMsg);
+                    writeToFile(ERROR_LOG_FILE, errorMsg);
+                } else if (pulseValue > HIGHT_BAD_PULSE || pulseValue < LOW_BAD_PULSE) {
+                    String warningMsg = "Bad pulse: " + pulseValue;
+                    logger.log("warning", warningMsg);
+                    writeToFile(ERROR_LOG_FILE, warningMsg);
+                }
+            }
+            
             socket.send(packet);
         }
-    };
+    }
+
+    private static void writeToFile(String fileName, String data) {
+        try (FileWriter writer = new FileWriter(fileName, true)) {
+            writer.write(data + "\n");
+        } catch (IOException e) {
+            logger.log("severe", "Error writing to file: " + fileName);
+        }
+    }
 }
